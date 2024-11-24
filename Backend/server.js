f2d3cb1f7b5857d561a1165bab8b0e3f77da71d9
Balasai Terli <138983969+BalasaiTerli@users.noreply.github.com>
2024-11-24 21:25:31 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,11 @@
 const express = require("express");
-const cors = require('cors');
-
 const dotenv = require("dotenv");
 const cors = require("cors")
 const dbConnection = require("./src/dbConnection");
 const signupController = require("./src/controllers/signup");
 const loginController = require("./src/controllers/login");
 const imageController = require("./src/controllers/Image"); // Add image controller
-
 const app = express();
-app.use(cors()); // Enable CORS for all routes
-app.use(express.json()); // Parse JSON bodies
-
 dotenv.config();
 app.use(express.json());
 app.use(cors())
@@ -20,7 +14,7 @@ const PORT = process.env.PORT || 8000;
 
 app.post("/signup", signupController);
 app.post("/login",loginController)
-app.get("/images", imageController.getImages);
+app.get("/:category", imageController.getImagesByCategory); 
 
 
 dbConnection(process.env.DB_URI)
