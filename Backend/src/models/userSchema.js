935a1b934a9f0657c61d1635308a165fe3d1303d
Balasai Terli <138983969+BalasaiTerli@users.noreply.github.com>
2024-11-25 12:52:56 +0530

    Update userSchema.js

--- Backend/src/models/userSchema.js
+++ Backend/src/models/userSchema.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 
-
 const userSchema = new mongoose.Schema({
-    email:{
-       type:String,
-       required:[true,"Email is required"],
-       unique:true,
-       trim:true,
-       lowercase:true
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true
     },
-    password:{
-        type:String,
-        required:[true,"Password is required"],
-        trim:true
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        trim: true
+    },
+    phoneNumber: {
+        type: String,
+        required: [true, "Phone number is required"],
+        trim: true
     }
-})
+});
 
-module.exports = mongoose.model("user",userSchema)
+module.exports = mongoose.model("user", userSchema);
