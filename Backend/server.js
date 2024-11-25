3cafa458f9686405fe59be3f0e83b414e66c872f
Sheshi8317665599 <vikrayaofficial@gmail.com>
2024-11-25 11:37:33 +0530

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require('cors');
+
 const dotenv = require("dotenv");
-const cors = require("cors")
+
 const dbConnection = require("./src/dbConnection");
 const signupController = require("./src/controllers/signup");
 const loginController = require("./src/controllers/login");
-const imageController = require("./src/controllers/Image");
+const imageController = require("./src/controllers/Image"); // Add image controller
+
 const app = express();
+
+
+
 dotenv.config();
 app.use(express.json());
 app.use(cors())
